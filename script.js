c78e881c26a44cae7dfd43601bba6dac5b765a33
j.bennett <61255945+jben817@users.noreply.github.com>
2020-05-24 21:54:32 -0400

    added functions, generator only return a random letter needs more work

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
-const form = document.getElementById('passGen') 
-const passwordLengthElement = document.getElementById ('passwordLength')
+const form = document.getElementById('passGen')
+const passwordLengthElement = document.getElementById('passwordLength')
 const includeLowerElement = document.getElementById('includeLower')
 const includeUpperElement = document.getElementById('includeUpper')
 const includeNumberElement = document.getElementById('includeNumber')
 const includeSymbolsElement = document.getElementById('includeSymbols')
 const blankBox = document.getElementById('blankBox')
+const generateIt = document.getElementById('generateIt')
 
-const lowerCaseCode = characterRange(97, 112)
-const upperCaseCode = characterRange(65, 90) 
-const numberCaseCode = characterRange(48, 57)
-const symbolCaseCode = characterRange(33, 47) 
-
-
-form.addEventListener('input', e => {
+generateIt.addEventListener('click', e => {
     e.preventDefault()
-const passwordLength = passwordLengthElement.input
+const passwordLength = parseInt(passwordLengthElement.value)
 const includeLower = includeLowerElement.checked
 const includeUpper = includeUpperElement.checked
 const includeNumber = includeNumberElement.checked
 const includeSymbols = includeSymbolsElement.checked
-const passGen = generatePassword (passwordLength, includeUpper, includeLower, includeNumber, includeSymbols)
-blankBox.innertext = passWordS})
+blankBox.innerText = generatePassword (passwordLength, includeUpper, includeLower, includeNumber, includeSymbols)
+});
+
+
 
 function generatePassword (passwordLength, includeUpper, includeLower, includeNumber, includeSymbols) {
-    var passwordPossibilities = lowerCaseCode
-    if(includeUpper) passwordPossibilities = passwordPossibilities.concat(upperCaseCode)
-    if(includeNumber) passwordPossibilities = passwordPossibilities.concat(numberCaseCode)
-    if(includeSymbols) passwordPossibilities = passwordPossibilities.concat(SymbolCaseCode)
-
-const passWordS = []
-for(var i = 8; i <= 120; i++){
-    const character = characterRange[Math.floor(Math.random() * passwordPossibilities.length)]
-    passWordS.push(String.fromCharCode(character))
+    if(includeLower === true)
+    return includeLowerCase();
+    if(includeUpper === true)
+    return includeUpperCase();
+    if(includeNumber === true)
+    return includeNumberFunc();
+    if(includeSymbols === true)
+    return includeSymbolFunc();
 }
 
-return passWordS.join("")
-}
+function includeLowerCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
 
-function characterRange (low, high) {
-const array = []
-for(var i =low; i <= high; i++ ) {
-    array .push(i)
-}
-return array
-}
+function includeUpperCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+function includeNumberFunc() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+function includeSymbolFunc() {
+    var symbols = "!@#$%^&*()+{}:?";
+    return symbols[Math.floor(Math.random() * 15)]; 
+};
+
